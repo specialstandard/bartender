06c94b818af751f3861ff849ad522026ee9a5d30
Brian <italissimo@gmail.com>
2016-08-04 22:42:18 +1000

    adds difficulty and start time preferences and preferences service

---
+++ preferences.service.js
@@ -0,0 +1,18 @@
+app.factory('preferencesService', ['$timeout', function( $timeout ){
+  return {
+    difficulty: 'easy',
+    startTime: 60,
+    setDifficulty: function ( difficulty ){
+      this.difficulty = difficulty
+    },
+    getDifficulty: function(){
+      return this.difficulty
+    },
+    setStartTime: function ( time ){
+      this.startTime = time
+    },
+    getStartTime: function(){
+      return this.startTime
+    }
+  }
+}])
